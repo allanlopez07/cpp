8d10df2b567b7732038b88e7ead80de824a1c710
Allan Lopez <allanlopezxd28@gmail.com>
2021-02-21 13:17:52 -0600

    examen

---
+++ Examenes/Proyecto I Unidad/Codigo/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include "startShip.h"
+#include "snake.h"
+
+
+
+using namespace std;
+
+string RetroJuegos [2][2]{
+
+{"1",   "startShip()"   },
+
+{"2",    "snake()"     }
+
+};
+
+
+int main(int argc, char const *argv[])
+{
+   
+
+    int opcion = 0;
+
+        system("cls");
+
+        while (true)
+        {
+            
+            cout << "********     JUEGOS CLASICOS     ********\n" << endl;
+
+            cout << "***   Seleccione un Juego     ***\n" << endl;
+
+            cout << "*****" << " 1   STARTSHIP   "<< "********" << endl;
+            cout << "*****" << " 2     SNAKE     "<< "********" << endl;
+
+            cout << "**  3   Salir   **\n" << endl;
+
+            cout << "Ingrese un numero para seleccionar un juego: ";
+
+            cin >> opcion;
+
+            switch (opcion)
+            {
+            case 1:
+                system("cls");
+                starShip();
+                
+                break;
+            
+            case 2:
+                system("cls");
+                snake();
+                
+                break;
+            
+            default:
+                break;
+            }
+
+            system("pause");
+                cout << endl;
+
+            if (opcion == 3)
+            {
+                break;
+            }
+            
+
+        }
+        
+
+    return 0;
+}
---
+++ Examenes/Proyecto I Unidad/README.md
@@ -0,0 +1,37 @@
+# Descargue el codigo del Proyecto de la Unidad I
+
+### 1) El objetivo es crear un menu en la funcion main para asi poder jugar el juego StarShip o Snake
+
+El menu debe contener estas opciones:
+
+
+```sh
+
+**************
+MENU DE JUEGOS
+**************
+
+Seleccione un juego
+
+1 - StarShip
+
+2 - Snake
+
+Ingrese un numero del menu para seleccionar un juego:
+
+```
+
+
+### 2) Para poder compilar el programa ejecute el comando
+
+`./compilar.bat`
+
+### 3) Vea este video para mas detalle en esta asignacion
+
+https://www.youtube.com/watch?v=hOGHh8dnbjg
+
+### 4) Entregables
+
+1) Link de github.com ***10%
+2) Video Youtube demostrando la funcionalidad de su sistema y el codigo que implemento para el menu ***7%
+
